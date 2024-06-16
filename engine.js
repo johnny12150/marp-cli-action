ee0c9ad2775d249394b6a052e265e430db6d032d
Jain Wun Su <johnny12150@gmail.com>
2024-06-16 19:56:44 +0800

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,4 +1,17 @@
 const { Marp } = require('@marp-team/marp-core')
 const highlightLines = require('markdown-it-highlight-lines')
 
-module.exports = (opts) => new Marp(opts).use(highlightLines)
+module.exports = (opts) => new Marp(opts)
+  .use(highlightLines)
+  .use(({ marpit }) => {
+      const { highlighter } = marpit
+  
+      // Override Marp Core's highlighter to wrap each lines by ordered list items
+      marpit.highlighter = (...args) => {
+        const original = highlighter(...args)
+        const listItems = original
+          .split(/\n(?!$)/) // Don't split at the trailing newline
+          .map((line) => `<li>${line}</li>`)
+  
+        return `<ol>${listItems.join('')}</ol>`
+    }
