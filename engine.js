6519068820ca922d8a8ef31511a87b8473700bd0
Jain Wun Su <johnny12150@gmail.com>
2024-06-16 17:30:46 +0800

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,4 +1,4 @@
 const { Marp } = require('@marp-team/marp-core')
-// const highlightLines = require('markdown-it-highlight-lines')
+const highlightLines = require('markdown-it-highlight-lines')
 
-// module.exports = (opts) => new Marp(opts).use(highlightLines)
+module.exports = (opts) => new Marp(opts).use(highlightLines)
