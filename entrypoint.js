6f3770a19294f5199fb957f9766d5610656ef1a9
Jain Wun Su <johnny12150@gmail.com>
2024-06-16 15:31:02 +0800

    Update entrypoint.js

--- entrypoint.js
+++ entrypoint.js
@@ -130,6 +130,8 @@ async function marpCli(userConfigFilePath, config) {
 
   await exec("node", [
     "/home/marp/.cli/marp-cli.js",
+    "--engine", 
+    "./engine.mjs"
     "--config-file",
     configPath,
     "-I",
